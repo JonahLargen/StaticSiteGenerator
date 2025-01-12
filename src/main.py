3a427a4ddc67fad15259f1f5dc17171c4863b92a
JonahLargen <jhlargen2@gmail.com>
2025-01-12 16:38:57 -0500

    Remove Unused

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
-from contents import copy_source_contents_to_destination, generate_page, generate_pages_recursive
+from contents import copy_source_contents_to_destination, generate_pages_recursive
 
 def main():
     copy_source_contents_to_destination("static", "public")
-    #generate_page('content/index.md', 'template.html', 'public/index.html')
     generate_pages_recursive('content', 'template.html', 'public')
         
 main()
